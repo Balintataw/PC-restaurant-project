bf7b1dff3535a239331ccb4f0ed75a4ecf7dc125
balintataw <balintataw2@gmail.com>
2018-03-15 15:32:48 -0700

    starting menu

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    
+    // carousel stuff
     var slideIndex = 0;
     showSlides();
     
@@ -17,4 +17,15 @@ $(document).ready(function() {
         slides[slideIndex-1].style.display = "block";  
         // setTimeout(showSlides, 3000); 
     }
+
+    $.get("https://json-data.herokuapp.com/restaurant/news/1", function(data){
+        var news = `
+        <div id="news">
+                <h3 class="title">${data.title}</h3>
+                <span class="date">${data.date_published}</span>
+                <p class="post">${data.post}</p>
+            </div>
+            `
+            $(".news").append(news)
+    })
 })
