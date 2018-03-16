d2c9609d59bdc4f7891382277ac963447547ed67
balintataw <balintataw2@gmail.com>
2018-03-15 19:34:02 -0700

    re-wrote carousel so it fades images properly

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 $(document).ready(function() {
 
-    // carousel stuff
-    var slideIndex = 0;
-    showSlides();
-    
-    function showSlides() {
-        
-        var slides = $(".mySlides");
-        
-        $.each(slides, function(i, item) {
-            item.style.display = "none";
-        })
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}    
-        
-        slides[slideIndex-1].style.display = "block";  
-        // setTimeout(showSlides, 3000); 
-    }
+    // carousel 
+
+    $(function(){   
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+    });
+
     // news section
     $.get("https://json-data.herokuapp.com/restaurant/news/1", function(data){
         var news = `
